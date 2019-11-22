c15b5aded24bb44e877f59bc16261765da67d5f4
Alexey Zakharchenko <zakh_stu@rambler.ru>
2019-11-22 17:58:52 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,8 +3,7 @@ var filesToCache = [
   '',
   'index.html',
   'css/style.css',
-  'js/main.js',
-  'https://www.gstatic.com/cv/js/sender/v1/cast_sender.js?loadCastFramework=1'
+  'js/main.js'
 ];
 
 /* Start the service worker and cache all of the app's content */
@@ -12,6 +11,24 @@ self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
       return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Delete old cache */
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          // Return true if you want to remove this cache,
+          // but remember that caches are shared across
+          // the whole origin
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
     })
   );
 });
